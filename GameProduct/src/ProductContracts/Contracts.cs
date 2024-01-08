5eed0fac9f8b24e509f3ee43c7e96814d496c66d
YuvalAvishid <avishidyuval@gmail.com>
2024-01-08 14:55:04 +0200

    ProductContracts Classlib
    Created ClassLib that is responsible for contracts.

---
+++ GameProduct/src/ProductContracts/Contracts.cs
@@ -0,0 +1,5 @@
+﻿namespace ProductContracts;
+
+public record ProductItemCreated(Guid ItemId, string Name, string Description);
+public record ProductItemUpdated(Guid ItemId, string Name, string Description);
+public record ProductItemDeleted(Guid ItemId);
